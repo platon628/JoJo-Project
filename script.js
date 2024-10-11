8ad5d7e05834dc52958f2a3df02f747691e65057
platon628 <platon.parv@tptlive.ee>
2024-10-11 09:55:48 +0300

    Added loop boxes and text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = "https://stand-by-me.herokuapp.com/api/v1/characters"
 
-async function loadCharacters(){
+/*async function loadCharacters(){
     fetch(url) 
         .then(response => response.json()) 
         .then(json => {
@@ -8,7 +8,7 @@ async function loadCharacters(){
                 json[i].id
                 console.log(json)
                 const characterText = GetCharacterText()
-                characterText.innerHTML = `
+                characterText.innerHTML += `
                 <p>${json[i].name}</p>
                 <p>${json[i].japaneseName}</p>
                 <p>${json[i].abilities}</p>
@@ -17,8 +17,31 @@ async function loadCharacters(){
 
         })
         .catch(error => console.error('Error fetching text:', error));
+  }*/
+  function BoxesCreate(){
+    fetch(url) 
+    .then(response => response.json()) 
+    .then(json => {
+        for(let j = 0; j<175;j++){
+            const container = GetCharacterText()
+            const box = document.createElement("div")
+            box.setAttribute("class","box")
+            box.innerHTML += `
+            <p>${json[j].name}</p>
+            <p>${json[j].japaneseName}</p>
+            <p>${json[j].abilities}</p>
+            <p>${json[j].nationality}</p>
+            <p>${json[j].catchphrase}</p>
+            <p>${json[j].chapter}</p>
+            `
+            container.appendChild(box)
+        }
+    })
+    .catch(error => console.error('Error fetching text:', error));
   }
+
   function GetCharacterText() {
-    return document.getElementsByClassName("box")[0]
+    return document.getElementsByClassName("container")[0]
 }
-  window.onload = loadCharacters;
+  //window.onload = loadCharacters;
+  window.onload = BoxesCreate;
